b99208448be3010e814b09b9f412fd9ec3ec340c
Carlo Pitogo <pitogojohncarlo50@gmail.com>
2025-05-21 01:50:56 +0800

    update api

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -11,7 +11,7 @@ const Login = ({ onLogin }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/login", {
+      const response = await fetch("https://f0d5-49-146-202-126.ngrok-free.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
